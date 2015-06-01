9e61e914e4fa8a7bcf5fab92536b371ed6a606bc
Eddie Hurtig <EdHurtig@users.noreply.github.com>
2015-06-01 14:57:23 -0400

    Default the KNIFE_NODE_NAME attribute to the node name

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,9 +2,9 @@ default['chef_stale_bot']['install_dir'] = '/usr/share/chef-bot'
 
 default['chef_stale_bot']['cron']['minute'] = '*/5'
 
-default['chef_stale_bot']['environment']['CHEF_SERVER_ENDPOINT'] = 'https://api.opscode.com/organizations/<org>'
-default['chef_stale_bot']['environment']['CHEF_BOT_SLACK_HOOK'] = '<slack-hook>'
-default['chef_stale_bot']['environment']['KNIFE_NODE_NAME'] = 'edhurtig'
+default['chef_stale_bot']['environment']['CHEF_SERVER_ENDPOINT'] = ''
+default['chef_stale_bot']['environment']['CHEF_BOT_SLACK_HOOK'] = ''
+default['chef_stale_bot']['environment']['KNIFE_NODE_NAME'] = node.name
 default['chef_stale_bot']['environment']['KNIFE_CLIENT_KEY'] = '/etc/chef/client.pem'
 default['chef_stale_bot']['environment']['CHEF_BOT_STALE_TIME'] = 3600
 default['chef_stale_bot']['environment']['CHEF_BOT_CHANNEL'] = '#ops-dev'
