5983eb4f84acac7729adc544c83043546eeae903
Eddie Hurtig <EdHurtig@users.noreply.github.com>
2015-06-02 09:30:27 -0400

    Bring default stale timeout attribute to 90 mins

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,5 +6,5 @@ default['chef_stale_bot']['environment']['CHEF_SERVER_ENDPOINT'] = ''
 default['chef_stale_bot']['environment']['CHEF_BOT_SLACK_HOOK'] = ''
 default['chef_stale_bot']['environment']['KNIFE_NODE_NAME'] = node.name
 default['chef_stale_bot']['environment']['KNIFE_CLIENT_KEY'] = '/etc/chef/client.pem'
-default['chef_stale_bot']['environment']['CHEF_BOT_STALE_TIME'] = 3600
+default['chef_stale_bot']['environment']['CHEF_BOT_STALE_TIME'] = 4800
 default['chef_stale_bot']['environment']['CHEF_BOT_CHANNEL'] = '#ops-dev'
